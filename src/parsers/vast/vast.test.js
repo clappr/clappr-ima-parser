5dfd9ca537216d0816077d7ebc11ad004ebe4dab
= <lucio.furtado@g.globo>
2024-08-19 15:53:26 -0300

    style: fix lint

--- src/parsers/vast/vast.test.js
+++ src/parsers/vast/vast.test.js
@@ -4,7 +4,7 @@ import { VASTClient } from '@dailymotion/vast-client'
 describe('VASTManager', () => {
   let VASTHandler
   const url = 'https://ad-server.com/test?vad_type=linear'
-  const vastIdInfoFromUrl2 = [{id: '452369852', sequence: null, adType: null, adServingId: null, categories: 'A'}]
+  const vastIdInfoFromUrl2 = [{ id: '452369852', sequence: null, adType: null, adServingId: null, categories: 'A' }]
 
   beforeEach(() => {
     VASTHandler = new VASTManager()
@@ -27,42 +27,46 @@ describe('VASTManager', () => {
         })
     })
 
-    it('returns a promise for a valid received adData', async () => {
+    it('returns a promise for a valid received adData', async() => {
       jest.spyOn(VASTHandler, '_requestVASTAdInformation').mockImplementation(() => new Promise(resolve => resolve()))
 
-      let response = VASTHandler.request({ '#cdata': url })
+      const response = VASTHandler.request({ '#cdata': url })
 
       await expect(response instanceof Promise).toBeTruthy()
     })
 
-    it('return a array of promise with a valid receive adData', async () => {
-      const vastIdInfoFromUrl1 = [{id: '1234568965', sequence: null, adType: null, adServingId: null, categories: 'A'}]
+    it('return a array of promise with a valid receive adData', async() => {
+      const vastIdInfoFromUrl1 = [{ id: '1234568965', sequence: null, adType: null, adServingId: null, categories: 'A' }]
 
-      const responseMock = [{id: '1234568965', sequence: null, adType: null, adServingId: null, categories: 'A'},
-        {id: '452369852', sequence: null, adType: null, adServingId: null, categories: 'A'}]
+      const responseMock = [
+        { id: '1234568965', sequence: null, adType: null, adServingId: null, categories: 'A' },
+        { id: '452369852', sequence: null, adType: null, adServingId: null, categories: 'A' },
+      ]
 
       jest.spyOn(VASTHandler, '_requestVASTAdInformation').mockImplementationOnce(() => new Promise(resolve => resolve(vastIdInfoFromUrl1)))
       jest.spyOn(VASTHandler, '_requestVASTAdInformation').mockImplementationOnce(() => new Promise(resolve => resolve(vastIdInfoFromUrl2)))
 
-      await expect(VASTHandler.request([{ '#cdata': url }, {'#cdata': url}])).resolves.toEqual(responseMock)
+      await expect(VASTHandler.request([{ '#cdata': url }, { '#cdata': url }])).resolves.toEqual(responseMock)
     })
 
-    it('return a valid promise even if one of the adData items contains an error', async () =>{
+    it('return a valid promise even if one of the adData items contains an error', async() => {
       const vastIdInfoErrorFromUrl1 = new Error('Empty ads')
 
-      jest.spyOn(VASTHandler, '_requestVASTAdInformation').mockImplementationOnce(() => new Promise(resolve => resolve(vastIdInfoErrorFromUrl1)))
+      jest.spyOn(VASTHandler, '_requestVASTAdInformation').mockImplementationOnce(
+        () => new Promise(resolve => resolve(vastIdInfoErrorFromUrl1)),
+      )
       jest.spyOn(VASTHandler, '_requestVASTAdInformation').mockImplementationOnce(() => new Promise(resolve => resolve(vastIdInfoFromUrl2)))
 
-      await expect(VASTHandler.request([{ '#cdata': url }, {'#cdata': url}])).resolves.toEqual(vastIdInfoFromUrl2)
+      await expect(VASTHandler.request([{ '#cdata': url }, { '#cdata': url }])).resolves.toEqual(vastIdInfoFromUrl2)
     })
 
-    it('returns an empty array if the ads is empty', async () => {
+    it('returns an empty array if the ads is empty', async() => {
       jest.spyOn(VASTHandler.client, 'get').mockImplementationOnce(() => new Promise(resolve => resolve({})))
 
       await expect(VASTHandler.request({ '#cdata': url })).resolves.toEqual([])
     })
 
-    it('returns the ad content after the promise is resolved', async () => {
+    it('returns the ad content after the promise is resolved', async() => {
       const responseMock = { ads: [{ creatives: [{ mediaFiles: {} }] }, { creatives: [{ mediaFiles: {} }] }] }
 
       jest.spyOn(VASTHandler.client, 'get').mockImplementationOnce(() => new Promise(resolve => resolve(responseMock)))
